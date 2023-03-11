25d086be0b657f0c7da58a353165cf462e194354
Masoud bahamin <Bahaminwp@gmail.com>
2023-03-12 01:43:48 +0300

    responsive and finish

--- script.js
+++ script.js
@@ -1,53 +1,62 @@
+let locationName = document.getElementById("location");
+let temp = document.getElementById("temp");
+let weatherName = document.getElementById("weather-name");
+let weather = document.getElementById("weather");
+let humidity = document.getElementById("humidity");
+let pressure = document.getElementById("pressure");
+let windSpeed = document.getElementById("wind-speed");
+let windDegree = document.getElementById("wind-degree");
+let feelsLike = document.getElementById("feels-like");
+let sunrise = document.getElementById("sunrise");
+let sunset = document.getElementById("sunset");
+let switchbtn = document.getElementById("switch");
+const modal = document.getElementById("modal");
+const btn = document.getElementById("btn");
+let date = document.getElementById("date");
+let clock = document.getElementById("clock");
+const months = ['Jan', 'Feb', 'mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+let city = "Istanbul";
+let weatherImage = "";
 
-window.addEventListener('load', getApi)
-
-let city = "Istanbul"
+window.addEventListener('load', getApi);
 
 function getApi() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bacb2bf4191872da42c94afbc7b155a5
-    `).then(res => res.json())
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bacb2bf4191872da42c94afbc7b155a5`)
+        .then(res => res.json())
         .then(data => {
-            console.log(data);
-            document.getElementById("location").innerText = ' ' + data.name
-            document.getElementById("temp").innerHTML = `${Math.floor(data.main.temp - 273)}<span>&#8451;</span>`
-            let img = ""
-            img = data.weather[0].main === "Rain" ? "rain.png" : data.weather[0].main === "Sun" ? "sun.png" : data.weather[0].main === "Clouds" ? "Clouds.png" : "sun1.png"
-            document.getElementById("weather-name").innerText = data.weather[0].main === "Rain" ? "rainy" : data.weather[0].main === "Sun" ? "suny" : data.weather[0].main === "Clouds" ? "Cloudy" : "sun cloudy"
-            document.getElementById("weather").innerHTML = `<img src="./image/${img}" class="main-img" id="weather"/> `
+            locationName.innerText = ' ' + data.name
+            temp.innerHTML = `${Math.floor(data.main.temp - 273)}<span>&#8451;</span>`
+            weatherImage = data.weather[0].main === "Rain" ? "rain.png" : data.weather[0].main === "Sun" ? "sun.png" : data.weather[0].main === "Clouds" ? "Clouds.png" : "sun1.png"
+            weatherName.innerText = data.weather[0].main === "Rain" ? "rainy" : data.weather[0].main === "Sun" ? "suny" : data.weather[0].main === "Clouds" ? "Cloudy" : "sun cloudy"
+            weather.innerHTML = `<img src="./image/${weatherImage}" class="main-img" id="weather"/> `
+            humidity.innerText = ' ' + Math.floor(data.main.humidity) + '%'
+            pressure.innerText = ' ' + Math.floor(data.main.pressure) + 'hPa'
+            windSpeed.innerText = ' ' + Math.floor(data.wind.speed) + 'km/h';
+            windDegree.innerText = ' ' + Math.floor(data.wind.deg);
+            feelsLike.innerHTML = `${Math.floor(data.main.feels_like - 273)}<span>&#8451;</span>`;
+            sunrise.innerText = '07:22';
+            sunset.innerText = "19:57"
 
-
-            document.getElementById("humidity").innerText = ' ' + Math.floor(data.main.humidity) + '%'
-            document.getElementById("pressure").innerText = ' ' + Math.floor(data.main.pressure) + 'hPa'
-            document.getElementById("wind-speed").innerText = ' ' + Math.floor(data.wind.speed) + 'km/h';
-            document.getElementById("wind-degree").innerText = ' ' + Math.floor(data.wind.deg);
-            document.getElementById("feels-like").innerHTML = `${Math.floor(data.main.feels_like - 273)}<span>&#8451;</span>`;
-            document.getElementById("sunrise").innerText = '07:22';
-            document.getElementById("sunset").innerText = "19:57"
-            let switchbtn = document.getElementById("switch");
             switchbtn.addEventListener('click', () => {
                 if (switchbtn.checked) {
-                    document.getElementById("temp").innerHTML = ` ${Math.floor((Math.floor(data.main.temp - 273)) * 9 / 5 + 32)}<span>&#8457;</span>`
+                    temp.innerHTML = ` ${Math.floor((Math.floor(data.main.temp - 273)) * 9 / 5 + 32)}<span>&#8457;</span>`
                 } else {
-                    document.getElementById("temp").innerText = ' ' + Math.floor(data.main.temp - 273) + "C"
+                    temp.innerHTML = `${Math.floor(data.main.temp - 273)}<span>&#8451;</span>`
                 }
-
             })
         })
 
-    let now = new Date();
-    let time = now.getHours();
+}
 
-    const months = ['Jan', 'Feb', 'mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+setInterval(() => {
+    let now = new Date();
+
     let hour = now.getHours().toString().padStart(2, "0");
     let minutes = now.getMinutes().toString().padStart(2, "0");
 
-    document.getElementById("date").innerHTML = `${months[now.getMonth() - 1]} ${now.getDate()}`
-    document.getElementById("clock").innerHTML = `${hour}:${minutes}`
-
-}
-
-const modal = document.getElementById("modal")
-const btn = document.getElementById("btn")
+    date.innerHTML = `${months[now.getMonth()]} ${now.getDate()}`
+    clock.innerHTML = `${hour}:${minutes}`
+}, 1000);
 
 btn.addEventListener("click", () => {
     modal.classList.toggle("modal-open")
